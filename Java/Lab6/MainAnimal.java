feaad8b13f61d05631dbc3784452be571ff241d2
AlexHelmutSonntag <61309161+AlexHelmutSonntag@users.noreply.github.com>
2020-10-25 14:48:22 +0100

    Add files via upload

---
+++ Java/Lab6/MainAnimal.java
@@ -0,0 +1,9 @@
+import animal.WildAnimal;
+
+public class MainAnimal {
+    public static void main(String[] args) {
+        System.out.println(WildAnimal.ELEPHANT.toString());
+        System.out.println(WildAnimal.listAllAnimals());
+    }
+
+}
